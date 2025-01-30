93372f83aa7349667aff87573e5e8e82cc6db14f
Daniel Bayley <daniel.bayley@me.com>
2025-01-30 11:26:11 +0000

    feat: main module

---
+++ index.js
@@ -0,0 +1,29 @@
+import {readdir} from "node:fs/promises"
+import path from "node:path"
+
+export const matchup = matchUp
+export async function matchUp(pattern, options = {}) {
+  options.max    ??= 10
+  options.ignore ??= ["node_modules"]
+
+  let cwd = options.cwd ?? import.meta.dirname
+
+  const rejoin = ({ parentPath, name }) => path.join(parentPath, name)
+  const ignore = entry =>
+    !options.ignore.some(part => rejoin(entry).includes(part))
+    && (options.symlinks === false ? !entry.isSymbolicLink() : true)
+
+  let depth = 1
+  while (depth <= options.max) {
+    const entries = await readdir(cwd, { withFileTypes: true })
+
+    const match = entries.filter(ignore)
+      .find(({ name }) => path.matchesGlob(name, pattern))
+
+    if (match) return path.parse(rejoin(match))
+    if (depth === options.max || cwd === path.sep) return {}
+
+    else cwd = path.resolve(cwd, "..")
+    depth += 1
+  }
+}
